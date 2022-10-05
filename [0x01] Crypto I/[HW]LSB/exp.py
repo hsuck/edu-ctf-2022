1d1b26f22390fe607f5d062ef621fabdeff602b1
hsuck <xmjj726716@gmail.com>
2022-10-05 16:17:03 +0800

    rearrange directories

---
+++ [0x01] Crypto I/[HW]LSB/exp.py
@@ -0,0 +1,31 @@
+from Crypto.Util.number import *
+from pwn import *
+
+p = remote( 'edu-ctf.zoolab.org', 10102 )
+
+n = int( p.recvline().decode().strip() )
+e = int( p.recvline().decode().strip() )
+enc = int( p.recvline().decode().strip() )
+print( n, e, enc )
+input('>')
+
+inv_3 = inverse( 3, n )
+i = 0
+b = 0
+pt = 0
+while True:
+    oracle = ( pow( inv_3, e * i, n ) * enc ) % n
+    p.sendline( str( oracle ).encode() )
+    r = int( p.recvline().decode().strip() )
+    x_i = ( r - ( inv_3 * b ) % n ) % 3
+
+    b = ( inv_3 * b + x_i ) % n
+    pt += x_i * ( 3 ** i )
+    print( r, x_i, i, pt )
+
+    i += 1
+
+    m = long_to_bytes( pt )
+    if b'flag{' in m or b'FLAG{' in m:
+        print( m )
+        break
